01b51803adfdb6af7603e9fde7c3c4055477b6f8
Vashish Esareesingh <vashishesaree2004@gmail.com>
2024-07-10 20:18:00 -0400

    Implemented Store Page Functionality

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        // Retrieve products from products table
+        $product_data = Product::all();
+
+        // Returns data to views/ productspage
+        return view('pages.testing.productspage', compact('product_data'));
+    }
 }
--- resources/views/pages/testing/productspage.blade.php
+++ resources/views/pages/testing/productspage.blade.php
@@ -5,12 +5,15 @@
 
     <div class="row">
 
+        @foreach ($product_data as $data)
+
         <div class="col-md-4">
             <img style="width: 200px; height: 200px" src="storage/images/products/iphone1-1.jpg" alt="image">
-            <p>Iphone 32</p>
-            <p>$500.00</p>
+            <p>{{ $data ->title }}</p>
+            <p>${{ $data->price }}</p>
             <p><a href="#">View</a></p>
         </div>
+        @endforeach
 
     </div>
 
