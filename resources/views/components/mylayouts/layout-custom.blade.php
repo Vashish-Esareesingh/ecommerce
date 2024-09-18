675a09b5210b234cd8536be3c0b8f5806c0cd043
Vashish Esareesingh <vashishesaree2004@gmail.com>
2024-09-17 21:41:44 -0400

    Chatbot change

--- resources/views/components/mylayouts/layout-custom.blade.php
+++ resources/views/components/mylayouts/layout-custom.blade.php
@@ -33,14 +33,21 @@
     <link rel="stylesheet" href="{{ asset('template_custom/assets/css/plugins/nouislider/nouislider.css') }}">
 
     {{-- Chatbot --}}
-    <script>
-        (function(w, d) { w.CollectId = "66c00fc0c7ec3e97bdf85084";
-        var h = d.head || d.getElementsByTagName("head")[0];
-        var s = d.createElement("script"); s.setAttribute("type", "text/javascript");
-        s.async=true; s.setAttribute("src", "https://collectcdn.com/launcher.js");
-        h.appendChild(s); })
-        (window, document);
+    <!-- Start of ChatBot (www.chatbot.com) code -->
+    <script type="text/javascript">
+        window.__be = window.__be || {};
+    window.__be.id = "66ea217632e5e40007e0890e";
+    (function() {
+        var be = document.createElement('script'); be.type = 'text/javascript'; be.async = true;
+        be.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.chatbot.com/widget/plugin.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(be, s);
+    })();
     </script>
+    <noscript>You need to <a href="https://www.chatbot.com/help/chat-widget/enable-javascript-in-your-browser/"
+            rel="noopener nofollow">enable JavaScript</a> in order to use the AI chatbot tool powered by <a
+            href="https://www.chatbot.com/" rel="noopener nofollow" target="_blank">ChatBot</a></noscript>
+    <!-- End of ChatBot code -->
+
 
 </head>
 
